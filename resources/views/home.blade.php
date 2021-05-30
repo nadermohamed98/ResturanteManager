0c1cbffea33af0ec87c79ea1e76f3d9c834bd0e8
Nader Mohamed <nadermohamed.19980@gmail.com>
2021-05-30 02:33:44 +0200

    home modefing

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,11 @@
 @section('title','Home')
 @section('content')
     <body class="HomeBody">
-        <h1>Welcome "{{Auth::user()->name}}" To <b>ParcDesPrinces</b> Restaurant website</h1>
-        <h3>Here You can find and order whatever you want from Different types of the food </h3>
+        <div class="hedding">
+            <h1>Welcome "{{Auth::user()->name}}" To <b>ParcDesPrinces</b> Restaurant website</h1>
+            <hr>
+            <h3>Here You can find and order whatever you want from Different types of the food </h3>
+        </div>
         <div id="cf">
             <div class="top">
                 <div class="Homeitem">
@@ -30,7 +33,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="Homeitem Homesecond">
+                <div class="Homeitem">
                     <div class="row">
                         <div class="col-md-4 col-sm-4">
                             <img style="width:170px;height:180px;border:1px solid;border-radius:250px" src="images/pizza.jfif">
@@ -42,7 +45,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="Homeitem Homesecond">
+                <div class="Homeitem">
                     <div class="row">
                         <div class="col-md-4 col-sm-4">
                             <img style="width:170px;height:180px;border:1px solid;border-radius:250px" src="/images/drinks.jfif">
@@ -57,50 +60,50 @@
             </div>
 
             <div class="bottom">
-                <div class="Homeitem">
+                <div class="Homesecond">
                     <div class="row">
                         <div class="col-md-4 col-sm-4">
-                            <img style="width:170px;height:180px;border:1px solid;border-radius:250px" src="/images/breakfast.jpg">
+                            <img style="width:170px;height:180px;border:1px solid;border-radius:250px" src="/images/hamburger.jpg">
                         </div>
                         <div class="col-md-6 col-sm-6">
-                            <h4>Name : hamburger</h4>
-                            <h4>Description :</h4><p>A light meal for breakfast you can eat and get your energy up and ready to GO</p>
+                            <h4>Name : Hamburger</h4>
+                            <h4>Description :</h4><p>A light meal for Dinner you can eat and get your energy up and ready to GO</p>
                             <h4>Price : <a href="/menu">Go to Menu</a></h4>
                         </div>
                     </div>
                 </div>
-                <div class="Homeitem">
+                <div class="Homesecond">
                     <div class="row">
                         <div class="col-md-4 col-sm-4">
-                            <img style="width:170px;height:180px;border:1px solid;border-radius:250px" src="images/steak.jfif">
+                            <img style="width:170px;height:180px;border:1px solid;border-radius:250px" src="images/sushi.jpg">
                         </div>
                         <div class="col-md-6 col-sm-6">
-                            <h4>Name : Steak</h4>
-                            <h4>Description :</h4><p>A delicious meal for Lunch made of tasty meat you can eat and get your energy up and ready to GO</p>
+                            <h4>Name : Sushi</h4>
+                            <h4>Description :</h4><p>A delicious meal for Dinner made of tasty meat you can eat and get your energy up and ready to GO</p>
                             <h4>Price : <a href="/menu">Go to Menu</a></h4>
                         </div>
                     </div>
                 </div>
-                <div class="Homeitem Homesecond">
+                <div class="Homesecond">
                     <div class="row">
                         <div class="col-md-4 col-sm-4">
-                            <img style="width:170px;height:180px;border:1px solid;border-radius:250px" src="images/pizza.jfif">
+                            <img style="width:170px;height:180px;border:1px solid;border-radius:250px" src="images/fish.jpg">
                         </div>
                         <div class="col-md-6 col-sm-6">
-                            <h4>Name : Pizza</h4>
+                            <h4>Name : Smooked Fish</h4>
                             <h4>Description :</h4><p>Pizza is a very delicious food you can eat and get your energy up and ready to GO</p>
                             <h4>Price : <a href="/menu">Go to Menu</a></h4>
                         </div>
                     </div>
                 </div>
-                <div class="Homeitem Homesecond">
+                <div class="Homesecond">
                     <div class="row">
                         <div class="col-md-4 col-sm-4">
-                            <img style="width:170px;height:180px;border:1px solid;border-radius:250px" src="/images/drinks.jfif">
+                            <img style="width:170px;height:180px;border:1px solid;border-radius:250px" src="/images/Lasagna.jpg">
                         </div>
                         <div class="col-md-6 col-sm-6">
-                            <h4>Name : Hot & Cold Drinks</h4>
-                            <h4>Description :</h4><p>There is a lots of drinks that you order and get your energy up and ready to GO</p>
+                            <h4>Name : Lasagna</h4>
+                            <h4>Description :</h4><p>A tasty dish of Lasagna to enjoy and get your energy up and ready to GO</p>
                             <h4>Price : <a href="/menu">Go to Menu</a></h4>
                         </div>
                     </div>
